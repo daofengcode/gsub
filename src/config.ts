ca1afc5c48502a2e36672f3249e20a0cea789829
daofengcode <daofengcode@protonmail.com>
2023-02-22 19:19:26 +0800

    移除废弃代码

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,6 @@
 import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector"
 import { buildAutocompleteQueryConfig, buildFacetConfigFromConfig, buildSearchOptionsFromConfig, getConfig } from "./helper"
 
-export const SEARCH_KEY = process.env.REACT_APP_SEARCH_KEY || ""
-export const ENDPOINT_BASE = process.env.REACT_APP_ENDPOINT_BASE || ""
-export const ENGINE_NAME = process.env.REACT_APP_ENGINE_NAME || ""
 export const VERSION = process.env.REACT_APP_VERSION || ""
 
 const { hostIdentifier, searchKey, endpointBase, engineName } = getConfig();
